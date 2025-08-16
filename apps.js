03569a0a8ac7c2785b53cbc2b12a273d89dd0fc7
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-15 23:30:36 -1200

    testing execution of multiple functions using onclick event

--- apps.js
+++ apps.js
@@ -2,9 +2,15 @@ let btns = document.querySelectorAll("button");
 
 for(btn of btns) {
     
-     btn.onclick = hello;
-      function hello() {
+     btn.onclick = sayHello;
+     btn.onclick = sayName;
+
+
+    function sayHello() {
         console.log("Hello, World!");
     }
+     function sayName() {
+        console.log("John Doe");
+    }
 
-}
+};
