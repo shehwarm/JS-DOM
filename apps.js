df568af210beb33251f99ea491fffddfd07d7048
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-16 17:05:12 +0500

    Activity using addEventListener

--- apps.js
+++ apps.js
@@ -1,27 +1,22 @@
-let btns = document.querySelectorAll("button");
+let btn = document.querySelector("button");
 
-for(btn of btns) {
 
-    btn.addEventListener("click", sayHello);
-    btn.addEventListener("click", sayGoodbye);
-    btn.addEventListener("dbclick", function() {
-        console.log("Double Clicked");
-    });
+btn.addEventListener("click", function(){
+    let h3 = document.querySelector("h3");
+    let randomColor = getRandomColor();
+    h3.innerText= randomColor;
 
-    function sayHello() {
-        console.log("Hello");
-    }
-    function sayGoodbye() {
-        console.log("Goodbye");
-    }
+    let div = document.querySelector("div");
+    div.style.backgroundColor = randomColor;
+
+    console.log("color updated");
+});
+
+function getRandomColor(){
+    let red= Math.floor(Math.random()*255);
+    let green= Math.floor(Math.random()*255);
+    let blue= Math.floor(Math.random()*255);
+
+    let color = `(${red}, ${green}, ${blue})`;
+    return color;
 }
-
-
-
-
-
-
-
-
-
-
