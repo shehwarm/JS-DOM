0c405796d4db344c72666d06fe0f64ae1d7374e5
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-16 18:30:40 +0500

    INPUT EVENT AND CHANGE EVENT

--- apps.js
+++ apps.js
@@ -1,17 +1,8 @@
-let inp = document.querySelector('input');
+let inp = document.querySelector("input");
+let p = document.querySelector("p");
 
 
-inp.addEventListener("keydown", function(event) {
-    console.log(event.code);
-    if (event.code === "ArrowUp") {
-        console.log("You pressed the up arrow key");
-    } else if (event.code === "ArrowDown") {
-        console.log("You pressed the down arrow key");
-    } else if (event.code === "ArrowLeft") {    
-        console.log("You pressed the left arrow key");
-    } else if (event.code === "ArrowRight") {
-        console.log("You pressed the right arrow key");
-    }
+inp.addEventListener("change", function(){
+    console.log(inp.value);
+    p.innerText = inp.value;
 });
-
- 
