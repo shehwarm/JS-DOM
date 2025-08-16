8f064e124b53737007878e100bda052517cecba5
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-15 23:23:57 -1200

    nmouseover event for multiple buttons

--- apps.js
+++ apps.js
@@ -2,14 +2,13 @@ let btns = document.querySelectorAll("button");
 
 for(btn of btns) {
     
-
      btn.onclick = hello;
-
       function hello() {
         console.log("Hello, World!");
     }
+
     btn.onmouseover = function() {
         console.log("Mouse over button");
-    }
+    };
 
 }
