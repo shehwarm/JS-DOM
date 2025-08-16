0c37600757d6728de7eb7508b7104df2405dbb08
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-16 17:31:42 +0500

    this in Event Listener

--- apps.js
+++ apps.js
@@ -1,22 +1,6 @@
-let btn = document.querySelector("button");
+let btn =   document.querySelector('button');
 
-
-btn.addEventListener("click", function(){
-    let h3 = document.querySelector("h3");
-    let randomColor = getRandomColor();
-    h3.innerText= randomColor;
-
-    let div = document.querySelector("div");
-    div.style.backgroundColor = randomColor;
-
-    console.log("color updated");
+btn.addEventListener('click', function() {
+  console.log(this.innerText);
+    this.style.backgroundColor = 'blue';
 });
-
-function getRandomColor(){
-    let red= Math.floor(Math.random()*255);
-    let green= Math.floor(Math.random()*255);
-    let blue= Math.floor(Math.random()*255);
-
-    let color = `(${red}, ${green}, ${blue})`;
-    return color;
-}
