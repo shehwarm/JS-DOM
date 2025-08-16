cf81665e0268534b9650dc752e90ed79753944df
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-16 00:15:25 -1100

    Onclick event

--- apps.js
+++ apps.js
@@ -1,15 +1,8 @@
-let box = document.createElement("div");
-let body = document.querySelector("body");
-body.appendChild(box);
-box.classList.add("box");
+let btn = document.querySelector("button");
+console.dir(btn);
 
-let head1 = document.createElement("h1");
-head1.textContent = "Im a div";
-box.appendChild(head1);
+function hello() {
+    console.log("Hello, World!");
+}
 
-let para1 = document.createElement("p");
-para1.textContent = "hey Im a paragraph inside a div";
-box.appendChild(para1);
-let para2 = document.createElement("p");
-para12.textContent = "hey Im another paragraph inside a div";
-box.appendChild(para2);
+btn.onclick = hello;
