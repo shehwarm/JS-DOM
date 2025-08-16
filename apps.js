8f886ae29efb2179c112919a1ee80beb2c75253d
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-16 17:40:36 +0500

    handling multiple events using this

--- apps.js
+++ apps.js
@@ -1,6 +1,14 @@
-let btn =   document.querySelector('button');
+let btn = document.querySelector('button');
+let p = document.querySelector('p');
+let h1 = document.querySelector('h1');
+let h3 = document.querySelector('h3');
 
-btn.addEventListener('click', function() {
-  console.log(this.innerText);
-    this.style.backgroundColor = 'blue';
-});
+function changeColor(){
+    console.dir(this.innerText);
+    this.style.backgroundColor = 'red';
+}
+
+btn.addEventListener('click', changeColor);
+h1.addEventListener('click', changeColor);
+h3.addEventListener('click', changeColor);
+p.addEventListener('click', changeColor);
