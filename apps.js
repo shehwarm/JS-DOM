6204017dc80ad6a05194f1f97aa77aa0c4d7631d
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-16 15:58:08 +0500

    created a div and then a heading inside it

--- apps.js
+++ apps.js
@@ -1,5 +1,12 @@
-let head1 = document.createElement("h1");
-head1.textContent = "Im a blue heading";
+let box = document.createElement("div");
 let body = document.querySelector("body");
-body.appendChild(head1);
-head1.classList.add("blue");
+body.appendChild(box);
+box.classList.add("box");
+
+let head1 = document.createElement("h1");
+head1.textContent = "Im a div";
+box.appendChild(head1);
+
+let para1 = document.createElement("p");
+para1.textContent = "hey Im a paragraph inside a div";
+box.appendChild(para1);
