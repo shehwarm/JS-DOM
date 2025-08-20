8cdfed139e4df1af886d0cf73eff21b8dbff486d
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-19 18:33:32 -1200

    call stack

--- apps.js
+++ apps.js
@@ -1,28 +1,9 @@
-let btn = document.querySelector("button");
-let inp = document.querySelector("input");
-let ul = document.querySelector("ul");
+function hello() {
+    console.log("hello");
+}
 
-btn.addEventListener("click", function(){
-    let item = document.createElement('li');
-    item.innerText = inp.value;
+function demo(){
+    hello();
+}
 
-    let delbtn = document.createElement("button");
-    delbtn.innerText = "delete";
-    delbtn.classList.add("delete");
-
-    item.appendChild(delbtn);
-    ul.appendChild(item);
-    console.log(inp.value);
-    inp.value = "";
-});
-
-
-ul.addEventListener("click", function(event){
-    if(event.target.nodeName == "BUTTON"){
-     let listItem = event.target.parentElement;
-     listItem.remove();
-     console.log("deleted");
-  
-    }
-    });
-
+demo(); 
