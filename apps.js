b213028ff00254917c2e9ef52102f60970bc4108
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-22 14:12:12 +0500

    First API request

--- apps.js
+++ apps.js
@@ -1,23 +1,3 @@
+let url = "https://catfact.ninja/fact";
 
-function delayValue() {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(10), 1000);
-  });
-}
-
-async function processValues() {
-  try {
-    let num = await delayValue();
-    console.log("First:", num);
-
-    num = num * 2;
-    console.log("Second:", num);
-
-    num = num * 3;
-    console.log("Third:", num);
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-
-processValues();
+fetch(url);
