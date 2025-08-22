cd8ca0a622af27bf1003131dcba62c03d1f70945
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-22 12:18:30 +0500

    Await Function

--- apps.js
+++ apps.js
@@ -1,18 +1,18 @@
-async function greet(){
-    throw "404 not found";
-    return "hello";
+function getNum(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            let num = Math.floor(Math.random()*10) + 1;
+            console.log(num);
+            resolve();
+        },1000);
+    });
 }
+        
+async function demo(){
+    await getNum();
+    await getNum();
+    await getNum();
+    getNum();
 
-greet()
-.then((result)=>{
-    console.log("success");
-    console.log("result was ", result);
-})
-.catch((err)=>{
-    console.log("promise rejected",err);
-});
 
-let demo = async () => {
-    return 5;
-
-};
+}  
