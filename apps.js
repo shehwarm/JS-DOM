27a49c1a074f8b1c35a3128629e23c58b787c807
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-21 21:06:08 -1200

    Using .then() and .catch() with fetch()

--- apps.js
+++ apps.js
@@ -1,7 +1,8 @@
-
-
-let student = {
-    name: "shehwar",
-    age:20,
-};
-
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then(response => response.json())
+  .then(data => {
+    console.log("Post Title:", data.title);
+  })
+  .catch(error => {
+    console.error("Error fetching data:", error);
+  });
