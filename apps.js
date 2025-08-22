286061eb0f986bbb6a27eb727e54da1e2f6c2f2d
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-22 12:38:00 +0500

    Async and Await Function

--- apps.js
+++ apps.js
@@ -1,18 +1,18 @@
-function getNum(){
+h1 = document.querySelector("h1");
+
+function changeColor(color, delay){
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
-            let num = Math.floor(Math.random()*10) + 1;
-            console.log(num);
-            resolve();
-        },1000);
-    });
-}
-        
-async function demo(){
-    await getNum();
-    await getNum();
-    await getNum();
-    getNum();
+            h1.style.color = color;
+            console.log(`color chnaged to $(color)`);
+            resolve("color changed");
+        },delay);
+        });
+    }
 
-
-}  
+    async function demo() {
+       await changeColor("red",1000);
+       await changeColor("orange",1000);
+       await changeColor("green",1000);
+        changeColor("blue",1000);
+    }
