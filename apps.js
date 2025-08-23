da03928384f64b06399a81283fc9e186cd5e180e
Shehwar Maryam <shehwarm93@gmail.com>
2025-08-23 16:04:24 +0500

    Simple Axios GET Request

--- apps.js
+++ apps.js
@@ -1,14 +1,12 @@
 let url = "https://catfact.ninja/fact";
 
-async function getFacts(){
-    try{
-      const config = { headers: {Accept: "applicaton/json"}};
-      let res = await axios.get(url, config);
-      
-      console.log(res.data);  
-
-    } catch(e){
-        console.log("erorr--",e);
-    }
-
+async function getCatFact() {
+  try {
+    let res = await axios.get(url);
+    console.log("Cat Fact:", res.data.fact);
+  } catch (e) {
+    console.log("Error:", e);
+  }
 }
+
+getCatFact();
